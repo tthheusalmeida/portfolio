0dfbf3216718c6439c2d4dd5b75b9397c87d225f
Matheus Almeida <dev.matheus.almeida@gmail.com>
2026-03-25 09:39:29 -0300

    feat: add firebase analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import HomeTemplate from "@/templates/HomeTemplate";
 
+import { useEffect } from "react";
+import { initAnalytics } from "@/services/firebase";
+
 export default function Home() {
+  useEffect(() => {
+    initAnalytics();
+  }, []);
+
   return <HomeTemplate />;
 }
